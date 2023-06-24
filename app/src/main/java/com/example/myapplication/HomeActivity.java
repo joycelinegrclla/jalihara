81c5d8686d5d7141cce122f19ff3febff12af90f
tesaieo <mangkangtesalonika@gmail.com>
2023-06-24 21:52:00 +0700

    login belum fix

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,2 +1,23 @@
-package com.example.myapplication;public class HomeActivity {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Mengambil username dari Intent
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("USERNAME");
+
+        // Menampilkan pesan selamat datang yang mengandung username
+        TextView welcomeTextView = findViewById(R.id.welcomeTextView);
+        welcomeTextView.setText("Welcome, " + username + "!");
+    }
 }
